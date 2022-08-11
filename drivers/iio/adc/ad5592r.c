228e7dc24931b99b1e7c1e0e1f6ca3b9f45477d6
Cristian Bindea <crisitan.bindea@analog.com>
2022-08-11 14:35:19 +0300

    drivers:iio:adc:ad5592r Added iio framework

--- drivers/iio/adc/ad5592r.c
+++ drivers/iio/adc/ad5592r.c
@@ -7,15 +7,33 @@
 
 #include <linux/module.h>
 #include <linux/spi/spi.h>
+#include <linux/iio/iio.h>
+
+static const struct iio_info ad5592r_info = {};
+
+static int ad5592r_probe(struct spi_device *spi)
+{
+	struct iio_dev *indio_dev;
+	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+
+	if (!indio_dev)
+		return -ENOMEM;
+
+	indio_dev->name = "ad5592r";
+	indio_dev->info = &ad5592r_info;
+
+	return devm_iio_device_register(&spi->dev, indio_dev);
+}
 
 static struct spi_driver ad5592r_driver = {
     .driver = {
         .name = "ad5592r",
     },
+    .probe = ad5592r_probe,
 };
 
 module_spi_driver(ad5592r_driver);
 
-MODULE_AUTHOR ("Bindea Cristian <cristian.bindea@analog.com>");
+MODULE_AUTHOR("Bindea Cristian <cristian.bindea@analog.com>");
 MODULE_DESCRIPTION("IIO Analog Devices AD5592r Drivers");
 MODULE_LICENSE("GPL_V2");
