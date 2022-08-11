041b256b6a2c811a1e706fb5947f5a896901399f
oance Sergiu <oance_sergiu@yahoo.com>
2022-08-11 17:15:03 +0300

    ad5592r changes for the bitstr

--- drivers/iio/adc/ad5592r.c
+++ drivers/iio/adc/ad5592r.c
@@ -4,20 +4,59 @@
  *
  * Copyright 2018 Analog Devices Inc.
  */
-#include <linux/module.h>
-#include <linux/spi/spi.h>
+
+
 #include <linux/iio/iio.h>
+#include <linux/module.h>
+#include <linux/regmap.h>
+#include <linux/spi/spi.h>
+
+
+struct ad5592r_state{
+    struct regmap *regmap
+};
+
+static int ad5592r_reg_access(struct iio_dev *indio_dev,
+				  unsigned reg, unsigned writeval,
+				  unsigned *readval)
+{
+    struct ad5592r_state *st = iio_priv(indio_dev);
+    if(readval)
+        return regmap_read(st->regmap, reg, readval);
+    return regmap_write(st->regmap, reg, writeval);
+}
 
 static const struct iio_info adi_ad5592r_info = {
+    .debugfs_reg_access = &ad5592r_reg_access,
+};
+
+static const struct regmap_config ad5592r_regmap_cfg={
+.reg_bits = 5,
+.val_bits = 11,
+
 
 };
 
+
 static int adi_ad5592r_probe(struct spi_device *spi)
 {
+
+       
        struct iio_dev *indio_dev;
-       indio_dev = devm_iio_device_alloc(&spi -> dev, 0);
-       if (!indio_dev)
+       struct ad5592r_state *st;
+       
+       indio_dev = devm_iio_device_alloc(&spi -> dev, sizeof(*st));
+       if (!indio_dev){
+
+       
         return -ENOMEM;
+       }
+st = iio_priv(indio_dev);
+st->regmap = regmap_init_spi(spi,&ad5592r_regmap_cfg );
+if(IS_ERR(st->regmap))
+    return PTR_ERR(st->regmap);
+      
+
        indio_dev->name = "iio-adi-ad5592r";
        indio_dev->info = &adi_ad5592r_info;
        dev_info(&spi->dev, "iio-adi-ad5592r Probed");
