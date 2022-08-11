dc61ca1c972de13a8276a7f23ad04ca1a7d36a0f
ceclandumitru <mitrutzceclan@gmail.com>
2022-08-11 14:34:01 +0300

    drivers:iio:adc ad5592 added iio framework driver registration

--- drivers/iio/adc/iio-ad5592.c
+++ drivers/iio/adc/iio-ad5592.c
@@ -7,15 +7,39 @@
 
 #include <linux/module.h>
 #include <linux/spi/spi.h>
+#include <linux/iio/iio.h>
+
+static struct iio_info iio_ad5592_info = {
+};
+
+static int iio_ad5592_probe(struct spi_device *spi){
+    struct iio_dev *indio_dev;
+
+    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+
+    if(indio_dev==NULL){
+        return -ENOMEM;
+    }
+
+    indio_dev->name = "iio-ad5592";
+    indio_dev->info = &iio_ad5592_info;
+
+    dev_info(&spi->dev, "iio_ad5592 Probed man");
+
+    return devm_iio_device_register(&spi->dev, indio_dev);
+    
+}
+
 
 static struct spi_driver iio_ad5592_driver = {
     .driver = {
         .name = "iio-ad5592",
-    }
+    },
+    .probe = iio_ad5592_probe,
 };
 
 module_spi_driver(iio_ad5592_driver);
 
 MODULE_AUTHOR("Ceclan Dumitru-Ioan <mitrutzceclan@gmail.com>");
-MODULE_DESCRIPTION("IIO Analog Devices emulator driver");
+MODULE_DESCRIPTION("IIO Analog Devices ad5592 driver");
 MODULE_LICENSE("GPL v2");
