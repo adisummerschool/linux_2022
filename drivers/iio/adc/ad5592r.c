e6f74958bddc147c2b511b5322f248166bb05934
Analog <bmaxim55@yahoo.com>
2022-08-11 14:33:55 +0300

    iio:adc:ad5592r: Driver AD5592R Signed-off-by: Maxim Bianca<bmaxim55@yahoo.com>

--- drivers/iio/adc/ad5592r.c
+++ drivers/iio/adc/ad5592r.c
@@ -1,21 +1,43 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
- * AD7124 SPI ADC driver
+ * AD5592R SPI ADC driver
  *
  * Copyright 2018 Analog Devices Inc.
  */
 
 #include <linux/module.h> 
 #include <linux/spi/spi.h>
+#include <linux/iio/iio.h>
 
-static struct spi_driver adi_ad5592r_driver = {
-    .driver = {
-        .name = "ad5592r",
-    }
+static const struct iio_info ad5592r_info = {
 };
 
-module_spi_driver(adi_ad5592r_driver);
+static int ad5592r_probe(struct spi_device *spi)
+{  
+       struct iio_dev *indio_dev;
+
+       indio_dev = devm_iio_device_alloc(&spi -> dev, 0);
+
+       if (!indio_dev)
+        return -ENOMEM;
+
+       indio_dev->name = "ad5592r";
+       indio_dev->info = &ad5592r_info; 
+       dev_info(&spi->dev, "ad5592r Probed");
+
+       return devm_iio_device_register(&spi->dev, indio_dev);
+}
+
+
+static struct spi_driver ad5592r_driver = {
+    .driver = {
+        .name = "ad5592r",
+    },
+    .probe = ad5592r_probe,
+};
+
+module_spi_driver(ad5592r_driver);
 
 MODULE_AUTHOR("Bianca Maxim <bmaxim55@yahoo.com>");
-MODULE_DESCRPIPTION("iio Analog Devices ad5592r Driver");
+MODULE_DESCRIPTION("iio Analog Devices ad5592r Driver");
 MODULE_LICENSE("GPL v2");
