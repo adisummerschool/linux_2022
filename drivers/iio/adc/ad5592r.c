4984eac02db1bde68a3170162690cb70c8044b9f
Muresan Mihai <mymihai22@yahoo.com>
2022-08-11 14:37:04 +0300

    ad5592r.c modified

--- drivers/iio/adc/ad5592r.c
+++ drivers/iio/adc/ad5592r.c
@@ -6,10 +6,29 @@
  */
 #include <linux/module.h>
 #include <linux/spi/spi.h>
+#include <linux/iio/iio.h>
+
+static const struct iio_info adi_ad5592r_info = {
+
+};
+
+static int adi_ad5592r_probe(struct spi_device *spi)
+{
+    struct iio_dev *indio_dev;
+    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+    if(!indio_dev)
+     return -ENOMEM;
+    indio_dev->name = "iio-adi-ad5592r";
+    indio_dev->info = &adi_ad5592r_info;
+    dev_info(&spi->dev, "iio-adi-ad5592r Probed");
+    return devm_iio_device_register(&spi->dev, indio_dev);
+}
+
 static struct spi_driver ad5592r_driver = {
     .driver = {
-        .name = "iio-adi-ad5592r",
-    }
+        .name = "ad5592r",
+    },
+    .probe = adi_ad5592r_probe,
 };
 module_spi_driver(ad5592r_driver);
 MODULE_AUTHOR("Muresan Mihai <mymihai22@yahoo.com>");
