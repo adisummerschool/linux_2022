c5e59d43e7ad23f9bd4de62d6aa652579736164f
Coroian Razvan <razvan.coroian@gmail.com>
2022-08-12 09:03:10 +0300

    Code corrected after finding conflicts on drivers for the same board

--- drivers/iio/adc/iio-adi-ad5592r.c
+++ drivers/iio/adc/iio-adi-ad5592r.c
@@ -1,37 +1,68 @@
-#include <linux/spi/spi.h>
-#include <linux/module.h>
 #include <linux/iio/iio.h>
+#include <linux/module.h>
+#include <linux/regmap.h>
+#include <linux/spi/spi.h>
 
-static const struct iio_info adi_ad5592r_info = {
-
+struct ad5592r_state {
+    struct regmap *regmap
 };
 
-static int adi_ad5592r_probe(struct spi_device *spi)
+static int ad5592r_reg_access(struct iio_dev *indio_dev, unsigned reg, unsigned writeval, unsigned *readval){
+    struct ad5592r_state *st = iio_priv(indio_dev);
+
+    if(readval)
+    {
+        return regmap_read(st->regmap, reg, readval);
+    }
+
+    return regmap_write(st->regmap, reg, writeval);
+}
+
+static const struct iio_info ad5592r_info = {
+    .debugfs_reg_access = ad5592r_reg_access,
+};
+
+static const struct regmap_config ad5592r_regmap_config = {
+    .reg_bits = 5,
+    .val_bits = 11,
+};
+
+static int ad5592r_probe(struct spi_device *spi)
 {
     struct iio_dev *indio_dev;
+    struct ad5592r_state *st;
 
-    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+    indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
     if(!indio_dev)
     {
         return -ENOMEM;
     }
     
-    indio_dev->name = "ad5592r";
-    indio_dev->info = &adi_ad5592r_info;
+    st = iio_priv(indio_dev);
 
-    dev_info(&spi->dev, "ad5592r Probed");
+    st->regmap = devm_regmap_init_spi(spi, &ad5592r_regmap_config);
+
+    if(IS_ERR(st->regmap))
+    {
+        return PTR_ERR(st->regmap);
+    }
+
+    indio_dev->name = "ad5592r-practica";
+    indio_dev->info = &ad5592r_info;
+
+    dev_info(&spi->dev, "ad5592r-practica Probe");
 
     return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
-static struct spi_driver adi_ad5592r_driver = {
+static struct spi_driver ad5592r_driver = {
     .driver = {
-        .name = "ad5592r",
+        .name = "ad5592r-practica",
     },
-    .probe = adi_ad5592r_probe,
+    .probe = ad5592r_probe,
 };
 
-module_spi_driver(adi_ad5592r_driver);
+module_spi_driver(ad5592r_driver);
 
 MODULE_AUTHOR("Coroian Razvan <razvan.coroian@gmail.com>");
 MODULE_DESCRIPTION("IIO Analog Devices Emulator Driver");
