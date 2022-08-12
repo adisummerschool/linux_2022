88ed5a5f39df12d7c40169b4c8863fa90eea23cf
Furnigel Andrei <dafurnigel@gmail.com>
2022-08-12 18:18:36 +0300

    Add change to ad5592r.c and renamed a variable in devicetree.dtb - in ad5592.c file it has been added 2 channels to read and write
    Signed-off-by: Furnigel Andrei <dafurnigel@gmail.com>

--- drivers/iio/adc/ad5592r.c
+++ drivers/iio/adc/ad5592r.c
@@ -2,32 +2,96 @@
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
 
-static const struct iio_info adi_adi5592r_info = {};
+struct adi_ad5592r_priv {
+	bool enable;
+};
+
+int adi_ad5592r_read_raw(struct iio_dev *indio_dev,
+		     struct iio_chan_spec const *chan, int *val, int *val2,
+		     long mask)
+{
+	struct adi_ad5592r_priv *priv = iio_priv(indio_dev);
+	switch (mask) {
+	case IIO_CHAN_INFO_RAW:
+		if (chan->channel)
+			*val = 2;
+		else
+			*val = 5;
+		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_ENABLE:
+		*val = priv->enable;
+		return IIO_VAL_INT;
+	}
+
+	return -EINVAL;
+}
+
+int adi_ad5592r_write_raw(struct iio_dev *indio_dev,
+		      struct iio_chan_spec const *chan, int val, int val2,
+		      long mask)
+{
+	struct adi_ad5592r_priv *priv = iio_priv(indio_dev);
+	switch (mask) {
+	case IIO_CHAN_INFO_ENABLE:
+		priv->enable = val;
+		return 0;
+	}
+
+	return -EINVAL;
+};
+
+static const struct iio_info adi_ad5592r_info = {
+	.read_raw = &adi_ad5592r_read_raw,
+	.write_raw = &adi_ad5592r_write_raw,
+};
+
+static const struct iio_chan_spec adi_ad5592r_channels[] = {
+	{
+		.type = IIO_VOLTAGE,
+		.output = 0,
+		.channel = 0,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+	},
+	{
+		.type = IIO_VOLTAGE,
+		.output = 0,
+		.channel = 1,
+		.indexed = 1,
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+	}    
+};
 
 static int adi_ad5592r_probe(struct spi_device *spi)
 {
-    struct iio_dev *indio_dev;
+	struct iio_dev *indio_dev;
+	struct adi_ad5592r_priv *priv;
 
-    indio_dev = devm_iio_device_alloc(&spi->dev,0);
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*priv));
+	if (!indio_dev)
+		return -ENOMEM;
 
-    if (!indio_dev) return -ENOMEM;
+	indio_dev->name = "ad5592r";
+	indio_dev->info = &adi_ad5592r_info;
+	indio_dev->channels = adi_ad5592r_channels;
+	indio_dev->num_channels = ARRAY_SIZE(adi_ad5592r_channels);
 
-    indio_dev->name ="adi5592r";
-    indio_dev->info = &adi_adi5592r_info;
-    dev_info(&spi->dev,"adi5592r Probed");
-    
-    return devm_iio_device_register(&spi->dev,indio_dev);  
+	priv = iio_priv(indio_dev);
+	priv->enable = false;
+
+	return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
-static struct spi_driver adi_adi5592r_driver = {
-    .driver = {
-        .name = "adi5592r",
-        
-    },
-    .probe = adi_ad5592r_probe,
-
+static struct spi_driver adi_ad5592r_driver = {
+	.driver = {
+		.name = "ad5592r",
+	},
+	.probe = adi_ad5592r_probe,
 };
-module_spi_driver(adi_adi5592r_driver);
+module_spi_driver(adi_ad5592r_driver);
+
 MODULE_AUTHOR("Furnigel Andrei <dafurnigel@gmail.com>");
 MODULE_DESCRIPTION("Analog Devices adi5592 Driver");
 MODULE_LICENSE("GPL v2");
