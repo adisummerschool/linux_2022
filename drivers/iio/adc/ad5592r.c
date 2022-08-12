31adc668fea990781c82373c287e44092cbae9a8
catalinsergiu <catalincurelaru0205@gmail.com>
2022-08-12 16:35:43 +0300

    ad5592r add canale

--- drivers/iio/adc/ad5592r.c
+++ drivers/iio/adc/ad5592r.c
@@ -8,53 +8,97 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
-#include<linux/regmap.h>
 
-struct ad5592r_state{
-	struct regmap *regmap
+struct ad5592r_priv{
+    bool enable;
 };
 
-static int ad5592r_reg_access(struct iio_dev *indio_dev,
-				  unsigned reg, unsigned writeval,
-				  unsigned *readval)
+int ad5592r_read_raw(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask)
 {
-    struct ad5592r_state *st = iio_priv(indio_dev);
-    if(readval)
-        return regmap_read(st->regmap, reg, readval);
-    return regmap_write(st->regmap, reg, writeval);
+    struct ad5592r_priv *priv=iio_priv(indio_dev);
+	switch (mask)
+    {
+        case IIO_CHAN_INFO_RAW:
+        if(chan->channel)
+        *val = 2;
+        else
+            *val=5;
+
+        return IIO_VAL_INT;
+
+        case IIO_CHAN_INFO_ENABLE:
+        *val=priv->enable;
+        return IIO_VAL_INT;
+                                   
+    }
+    return -EINVAL;
 }
+
+
+int ad5592r_write_raw(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int val,
+			int val2,
+			long mask)
+{
+    struct ad5592r_priv *priv=iio_priv(indio_dev);
+    switch (mask){
+        case IIO_CHAN_INFO_ENABLE:
+        priv->enable=val;
+        return 0;
+    }
+    return -EINVAL;
+}            
+
 static const struct iio_info ad5592r_info = {
-    .debugfs_reg_access = &ad5592r_reg_access
+    .read_raw = &ad5592r_read_raw,
+    .write_raw= &ad5592r_write_raw,
 };
 
 
+static const struct iio_chan_spec ad5592r_channels[]={
+    {
+    .type= IIO_VOLTAGE,
+    .output=0,
+    .channel=0,
+    .indexed=1,
+    .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+    },
 
-static const struct regmap_config ad5592r_regmap_cfg ={
-	.reg_bits=5,
-	.val_bits=11,
+    {
+    .type= IIO_VOLTAGE,
+    .output=0,
+    .channel=1,
+    .indexed=1,
+    .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+    }
 };
-
 
 static int ad5592r_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
-	struct ad5592r_state *st;
+	struct ad5592r_priv *priv;
 
-	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
+	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*priv));
 	if (!indio_dev)
 	{
 		return -ENOMEM;
 	}
 
-	st=iio_priv(indio_dev);
-	st->regmap=regmap_init_spi(spi,&ad5592r_regmap_cfg);
-
-	if (IS_ERR(st->regmap))
-	return PTR_ERR(st->regmap);
-
+	
 
 	indio_dev->name = "ad5592r";
 	indio_dev->info = &ad5592r_info;
+	indio_dev->channels = ad5592r_channels;
+    indio_dev->num_channels = ARRAY_SIZE(ad5592r_channels);
+    priv=iio_priv(indio_dev);
+    priv->enable = false;
 
 	dev_info(&spi->dev, "ad5592r Probed");
 
