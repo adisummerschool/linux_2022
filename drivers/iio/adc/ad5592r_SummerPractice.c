c8daab62981cc58e33aec8044309418a4d7872a5
Muresan Mihai <mymihai22@yahoo.com>
2022-08-16 17:03:30 +0300

    changes made in ad5592r_SummerPractice

--- drivers/iio/adc/ad5592r_SummerPractice.c
+++ drivers/iio/adc/ad5592r_SummerPractice.c
@@ -6,13 +6,16 @@
  */
 #include <asm/unaligned.h>
 #include <linux/bitfield.h>
-#include <linux/spi/spi.h>
-#include <linux/module.h>
+#include <linux/delay.h>
 #include <linux/iio/iio.h>
+#include <linux/module.h>
+#include <linux/spi/spi.h>
 
 #define ADI_AD5592R_REG_READBACK	0x7
 #define   ADI_AD5592R_MASK_RB_EN	BIT(6)
-#define   ADI_AD5592R_MASK_RB_REG	GENMASK(5, 2)
+#define   ADI_AD5592R_MASK_REG_RB	GENMASK(5, 2)
+#define ADI_AD5592R_REG_RESET		0XF
+#define   ADI_AD5592R_VAL_RESET		0x5AC
 
 #define ADI_AD5592R_ADDR_MASK		GENMASK(14, 11)
 #define ADI_AD5592R_VAL_MASK		GENMASK(10, 0)
@@ -59,7 +62,7 @@ static int adi_ad5592r_read_ctr(struct adi_ad5592r_state *st,
 
 	msg |= FIELD_PREP(ADI_AD5592R_ADDR_MASK, ADI_AD5592R_REG_READBACK);
 	msg |= ADI_AD5592R_MASK_RB_EN;
-	msg |= FIELD_PREP(ADI_AD5592R_MASK_RB_REG, reg);
+	msg |= FIELD_PREP(ADI_AD5592R_MASK_REG_RB, reg);
 
 	put_unaligned_be16(msg, &tx);
 
@@ -113,9 +116,30 @@ int adi_ad5592r_write_raw(struct iio_dev *indio_dev,
 	return 0;
 }
 
+static int adi_ad5592r_reg_access(struct iio_dev *indio_dev,
+				unsigned reg, unsigned writeval,
+				unsigned *readval)
+{
+	struct adi_ad5592r_state *st = iio_priv(indio_dev);
+	u16 read;
+	int ret;
+	if (readval){
+		ret = adi_ad5592r_read_ctr(st, reg, &read);
+		if (ret){
+			dev_err(&st->spi->dev, "DBG read failed");
+			return ret;
+		}
+		*readval = read;
+		return ret;
+	}
+	
+	return adi_ad5592r_write_ctr(st, reg, writeval);
+}
+
 static const struct iio_info adi_ad5592r_info = {
 	.read_raw = &adi_ad5592r_read_raw,
 	.write_raw = &adi_ad5592r_write_raw,
+	.debugfs_reg_access  = &adi_ad5592r_reg_access
 };
 
 static const struct iio_chan_spec adi_ad5592r_channels[] = {
@@ -153,10 +177,28 @@ static const struct iio_chan_spec adi_ad5592r_channels[] = {
 	}
 };
 
+static int adi_ad5592r_init(struct iio_dev *indio_dev)
+{
+	struct adi_ad5592r_state *st = iio_priv(indio_dev);
+	int ret;
+
+	//reset
+	ret = adi_ad5592r_write_ctr(st, ADI_AD5592R_REG_RESET, 
+				ADI_AD5592R_VAL_RESET);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Reset Failed");
+		return ret;
+	}
+	usleep_range(250, 300);
+	return 0;
+}
+
 static int adi_ad5592r_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
 	struct adi_ad5592r_state *st;
+	int ret;
 
 	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
 	if (!indio_dev) {
@@ -171,6 +213,13 @@ static int adi_ad5592r_probe(struct spi_device *spi)
 
 	st->spi = spi;
 
+	ret = adi_ad5592r_init(indio_dev);
+	if(ret)
+	{
+		dev_err(&st->spi->dev, "Reset Failed");
+		return ret;
+	}
+
 	dev_info(&spi->dev, "iio-adi-ad5592r Probed");
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
@@ -178,7 +227,7 @@ static int adi_ad5592r_probe(struct spi_device *spi)
 
 static struct spi_driver ad5592r_driver = {
 	.driver = {
-	.name = "ad5592r",
+	.name = "ad5592r_summer",
 	},
 	.probe = adi_ad5592r_probe,
 };
