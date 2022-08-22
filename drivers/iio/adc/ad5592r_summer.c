97e8bcd8ebe299f7b34567e0a2deccdb18c35762
darianaroman <alisadariana@gmail.com>
2022-08-22 10:33:48 +0300

    iio:adc:ad5592r_s Add read_raw and write_raw functions

--- drivers/iio/adc/ad5592r_summer.c
+++ drivers/iio/adc/ad5592r_summer.c
@@ -2,35 +2,71 @@
 #include <linux/module.h>
 #include <linux/iio/iio.h>
 
-static const struct iio_info adi_ad5592r_info = {
+static int adi_ad5592r_read_raw(struct iio_dev *indio_dev,
+				struct iio_chan_spec const *chan,
+				int *val,
+				int *val2,
+				long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_ENABLE:
+		*val = 0;
+		return IIO_VAL_INT;
+	case IIO_CHAN_INFO_RAW:
+		if(chan->channel)
+			*val = 0;
+		else
+			*val = 0;
+		return IIO_VAL_INT;
+	}
 
+	return -EINVAL;
+}
+
+static int adi_ad5592r_write_raw(struct iio_dev *indio_dev,
+				 struct iio_chan_spec const *chan,
+			 	 int val,
+			 	 int val2,
+			 	 long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_ENABLE:
+		return 0;
+	}
+
+	return -EINVAL;
+}
+
+static const struct iio_info adi_ad5592r_info = {
+	.read_raw = &adi_ad5592r_read_raw,
+	.write_raw = &adi_ad5592r_write_raw,
 };
 
 static const struct iio_chan_spec adi_ad5592r_channels[] = {
 	{
 		.type = IIO_VOLTAGE,
-		.info_mask_separate(IIO_CHAN_INFO_RAW),
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 		.output = 0,
 		.indexed = 1,
 		.channel = 0,
 	},
 	{
 		.type = IIO_VOLTAGE,
-		.info_mask_separate(IIO_CHAN_INFO_RAW),
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 		.output = 0,
 		.indexed = 1,
 		.channel = 1,
 	},
 	{
 		.type = IIO_VOLTAGE,
-		.info_mask_separate(IIO_CHAN_INFO_RAW),
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 		.output = 0,
 		.indexed = 1,
 		.channel = 2,
 	},
 	{
 		.type = IIO_VOLTAGE,
-		.info_mask_separate(IIO_CHAN_INFO_RAW),
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
 		.output = 0,
 		.indexed = 1,
 		.channel = 3,
@@ -47,7 +83,7 @@ static int adi_ad5592r_probe(struct spi_device *spi)
 	}
 
 	indio_dev->name = "ad5592r";
-	indio_dev->channels = &adi_ad5592r_channels;
+	indio_dev->channels = adi_ad5592r_channels;
 	indio_dev->num_channels = ARRAY_SIZE(adi_ad5592r_channels);
 	indio_dev->info = &adi_ad5592r_info; 
 
